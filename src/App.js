1205b462439eed33b8800caa230fedc0bf4f70a5
Degisew Mengist <you@example.com>
2022-09-14 18:58:25 +0300

    Converted function based component to class based

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+/* eslint-disable */
+import React, { Component } from 'react';
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
-
-export default App;
