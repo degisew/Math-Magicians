c7e740aa3edfee3e77996b0b27a205d2c0f9f159
Degisew <jegisew21@gmail.com>
2022-10-28 16:41:35 +0300

    fixed linters issue

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="header-title">Hello React</h1>
+        <h1 className="header-title">Hello from React</h1>
       </header>
     </div>
   );
